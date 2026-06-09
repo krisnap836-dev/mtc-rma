14efc89e02bad17e993c240e432c0acf0c113c0c
krisnap836-dev <krisnap836@gmail.com>
2026-06-09 17:49:30 +0700

    Update Firebase configuration
    Updated Firebase configuration with new project details.

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs, setDoc, deleteDoc, doc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBclk-4IY8ihoGSdQrLil1Kmbvq0Fweg2Y",
-  authDomain: "mtc-dashboard-rma.firebaseapp.com",
-  projectId: "mtc-dashboard-rma",
-  storageBucket: "mtc-dashboard-rma.firebasestorage.app",
-  messagingSenderId: "209882606718",
-  appId: "1:209882606718:web:5e5712e28c56b8599afc2f"
+  apiKey: "AIzaSyAN7wD1jNrV_a5owR-1vMJRrVy2OFOvF_E",
+  authDomain: "mtcrma-892a2.firebaseapp.com",
+  projectId: "mtcrma-892a2",
+  storageBucket: "mtcrma-892a2.firebasestorage.app",
+  messagingSenderId: "763912940438",
+  appId: "1:763912940438:web:93cd3b4e1d9eadddbb8bb7",
 };
 
 const app = initializeApp(firebaseConfig);
