410feb0caf3cff668153fdc0dd28441118cc8724
krisnap836-dev <krisnap836@gmail.com>
2026-06-14 00:03:34 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -340,7 +340,7 @@ function Dashboard({ downtimes, theme }) {
             <Tooltip {...tt} formatter={(v, n) => [n==="menit"?`${v} menit`:`${v} kejadian`, n==="menit"?"Total Menit":"Total"]}/>
             <Legend wrapperStyle={{ color: T.textSub, fontSize:12 }}/>
             <Bar dataKey="menit" name="Menit" radius={[6,6,0,0]}>{catData.map(e => <Cell key={e.name} fill={"#f59e0b"}/>)}</Bar>
-            <Bar dataKey="count" name="Kejadian" fill={#5cbcea} radius={[6,6,0,0]}/>
+            <Bar dataKey="count" name="Kejadian" fill={"#5cbcea"} radius={[6,6,0,0]}/>
           </BarChart>
         </ResponsiveContainer>
       </div>
